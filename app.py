dea26d02f60d5a43fe5baa5044565251961889c5
Elliot Alderson <60684641+0x0elliot@users.noreply.github.com>
2021-04-08 12:55:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import requests
+from flask import Flask, request
+from flask_ngrok import run_with_ngrok
+
+app=Flask(__name__)
+run_with_ngrok(app)
+
+@app.route('/')
+def hello():
+    discord_webhook_link="https://google.com"
+    data={'content':'all the shit you need. thoda bahut docs dekh le hojayga.'}
+    requests.post(discord_webhook_link, data=data)
+    return "5 rupai ki pepsi elliot bhai sexy."
+
+if __name__=="__main__":
+    app.run()
