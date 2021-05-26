2c78c4e775eb90347b89579c128c7b32029e9c68
Elliot Alderson <60684641+0x0elliot@users.noreply.github.com>
2021-05-27 00:25:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ run_with_ngrok(app)
 
 @app.route('/')
 def hello():
-    discord_webhook_link="https://google.com"
-    data={'content':'all the shit you need. thoda bahut docs dekh le hojayga.'}
-    requests.post(discord_webhook_link, data=data)
+    #discord_webhook_link="https://google.com"
+    #data={'content':'all the shit you need. thoda bahut docs dekh le hojayga.'}
+    #requests.post(discord_webhook_link, data=data)
     return "5 rupai ki pepsi elliot bhai sexy."
 
 if __name__=="__main__":
